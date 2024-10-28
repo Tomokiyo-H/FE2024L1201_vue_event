b7a3faa074b1b8aa9a63e497abecf76283b8c72c
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-10-28 10:24:20 +0900

    途中経過（リセットまで）

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,18 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count:0
         };
+    },
+    methods:{
+        increment(){
+            this.count*2;
+        },
+        decrement(){
+            this.count/2;
+        },
+        reset(){
+            this.count=1;
+        },
     }
 });
