08ed953093367208d1aac028895d1231df50e9f1
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-10-28 10:49:46 +0900

    完成

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,18 +3,25 @@ new Vue({
     el: '#app',
     data() {
         return {
-            count:0
+            count:1,
+            num:0
         };
     },
     methods:{
         increment(){
-            this.count*2;
+            this.count=this.count*2;
         },
         decrement(){
-            this.count/2;
+            this.count=this.count/2;
         },
         reset(){
             this.count=1;
         },
+        add(){
+            this.count=this.count+this.num;
+        },
+        decrease(){
+            this.count=this.count-this.num;
+        }
     }
 });
